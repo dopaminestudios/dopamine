cff295e95254c8a12d1562f7d02b6c2db143fe17
likerofturtles <singhdivyam664@gmail.com>
2026-02-19 15:21:19 +0530

    Added ability to reload package.

--- cogs/reload_me.py
+++ cogs/reload_me.py
@@ -1,33 +1,41 @@
 import discord
 import importlib
 import config
+import dopamine_framework
 from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+
+
 class Reload(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="rs", description=".")
+    @app_commands.command(name="rs", description="Reloads config and dopamine-framework")
     async def reload(self, interaction: discord.Interaction):
         if not await self.bot.is_owner(interaction.user):
             await interaction.response.send_message("🤫", ephemeral=True)
             return
+
         load_dotenv(override=True)
         importlib.reload(config)
+        importlib.reload(dopamine_framework)
+
         await interaction.response.send_message("👍️", ephemeral=True)
 
     @commands.command(name="rs")
-    async def reload(self, ctx: commands.Context):
+    async def reload_prefix(self, ctx: commands.Context):
         if not await self.bot.is_owner(ctx.author):
             await ctx.send("🤫", delete_after=5)
             return
 
         load_dotenv(override=True)
         importlib.reload(config)
+        importlib.reload(dopamine_framework)
 
         await ctx.send("👍️")
+
 
 async def setup(bot):
     await bot.add_cog(Reload(bot))
